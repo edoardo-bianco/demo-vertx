1f0373749382c1550e4ee8af8018a20db9302052
edoardo-bianco <edoardo.bianco@gmail.com>
2022-03-14 07:46:24 -0300

    chapter 2 demos

---
+++ src/main/java/it/dadus/demo/chapter2/deploy/Main.java
@@ -0,0 +1,11 @@
+package it.dadus.demo.chapter2.deploy;
+
+import io.vertx.core.Vertx;
+
+public class Main  {
+
+  public static void main(String[] args) {
+    Vertx vertx = Vertx.vertx();
+    vertx.deployVerticle(new Deployer());
+  }
+}
